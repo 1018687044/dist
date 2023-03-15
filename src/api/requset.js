7a59710c28f5767dbc4ed541481e0aaa7831a6e3
hcp <1018687044@qq.com>
2023-03-15 13:59:54 +0800

    shopping website

--- src/api/requset.js
+++ src/api/requset.js
@@ -11,10 +11,13 @@ import "nprogress/nprogress.css";
 
 // 1.利用axios对象的方法create 去创建一个axios实例
 // 2.request就是axios 只不过稍微配置一下
+
+let baseURL = " http://gmall-h5-api.atguigu.cn";
 const requests = axios.create({
   // 配置对象
   // 基础路径，发送请求的时候，路径当中会出现api
-  baseURL: "/api",
+  // baseURL: "/api",
+  baseURL: baseURL,
   //   代表请求超时的时间5s
   timeout: 5000,
 });
